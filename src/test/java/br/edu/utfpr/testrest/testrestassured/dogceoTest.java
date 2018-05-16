25cb215260dd13028a29f08c538997d479ace37d
Roberto <juninhoguimar@yahoo.com.br>
2018-05-16 17:47:23 -0300

    Atividade 18- Dog

---
+++ src/test/java/br/edu/utfpr/testrest/testrestassured/dogceoTest.java
@@ -0,0 +1,45 @@
+package br.edu.utfpr.testrest.testrestassured;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import io.restassured.parsing.Parser;
+import io.restassured.specification.ProxySpecification;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
+import org.junit.BeforeClass;
+
+public class dogceoTest {
+
+    @BeforeClass
+    public static void beforeClass() {
+        RestAssured.registerParser("text/plain", Parser.JSON);
+    }
+
+    @Test
+    public void dogAleatorio() {
+        when().
+                get("https://dog.ceo/api/breeds/image/random").
+                then()
+                .statusCode(200).
+                body("status", is("success"));
+
+    }
+
+    @Test
+    public void dogListarRacas() {
+        when().
+                get("https://dog.ceo/api/breed/hound/images").
+                then()
+                .statusCode(200).
+                body("status", is("success"));
+    }
+
+    @Test
+    public void dodListarSubracas() {
+        when().
+                get("https://dog.ceo/api/breed/hound/list").
+                then()
+                .statusCode(200).
+                body("status", is("success"));
+    }
+}
